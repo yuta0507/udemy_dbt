321c2eed042906623a5f045e14dc7b964607a90a
Yuta Kikkawa <yutakikkawa0507@gmail.com>
2025-12-22 03:09:22 +0000

    Add functions to refactor the CASE sentences

--- models/date_dimension.sql
+++ models/date_dimension.sql
@@ -4,22 +4,9 @@ WITH CTE AS (
         DATE(TO_TIMESTAMP(STARTED_AT)) AS DATE_STARTED_AT,
         HOUR(TO_TIMESTAMP(STARTED_AT)) AS HOUR_STARTED_AT,
 
-        CASE
-            WHEN DAYNAME(TO_TIMESTAMP(STARTED_AT)) IN ('Sat', 'Sun')
-            THEN 'WEEKEND'
-            ELSE 'BUSINESSDAY'
-        END AS DAY_TYPE,
+        {{ get_day_type('STARTED_AT') }} AS DAY_TYPE,
 
-        CASE
-            WHEN MONTH(TO_TIMESTAMP(STARTED_AT)) IN (12, 1, 2)
-            THEN 'WINTER'
-            WHEN MONTH(TO_TIMESTAMP(STARTED_AT)) IN (3, 4, 5)
-            THEN 'SPRING'
-            WHEN MONTH(TO_TIMESTAMP(STARTED_AT)) IN (6, 7, 8)
-            THEN 'SUMMER'
-            WHEN MONTH(TO_TIMESTAMP(STARTED_AT)) IN (9, 10, 11)
-            THEN 'AUTUMN'
-        END AS STATION_OF_YEAR
+        {{ get_season('STARTED_AT') }} AS STATION_OF_YEAR
 
     FROM
         {{ source('demo', 'bike') }}
